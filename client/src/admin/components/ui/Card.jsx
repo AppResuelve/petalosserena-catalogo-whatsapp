77f7f693b17edf9632617df74a2b2b6f51b6f496
AppResuelve <appresuelveok@gmail.com>
2026-06-28 19:19:50 -0300

    chore: eliminar proyecto Vite (reemplazado por store-next)

--- client/src/admin/components/ui/Card.jsx
+++
@@ -1,7 +0,0 @@
-export function Card({ children, className = '' }) {
-  return (
-    <div className={`bg-zinc-900 border border-zinc-800 rounded-xl p-6 ${className}`}>
-      {children}
-    </div>
-  )
-}
--- client/src/store/components/ui/index.js
+++
@@ -1,4 +0,0 @@
-export { ButtonPrimary, ButtonSecondary, ButtonWhatsApp } from './Button'
-export { Badge } from './Badge'
-export { SectionHeader } from './SectionHeader'
-export { FloatingWhatsAppButton } from './FloatingWhatsAppButton'
